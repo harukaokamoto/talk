c4dcd3e3ef109ddd0ec4b4962d23053e7832db6d
harukaokamoto <127079224+harukaokamoto@users.noreply.github.com>
2023-11-27 10:22:33 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+package_name = 'devel'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='uchida',
+    maintainer_email='uchida@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        'pub = devel.20231016_pub_test:main',
+        'sub = devel.20231016_sub_test:main',
+        'speech = devel.sub_speech:main',
+        ],
+    },
+)
